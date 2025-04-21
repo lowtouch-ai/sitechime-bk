236260db60237da4900f0b7ab9ab39878a110dcc
Sachin <sachin.george@pcpl.io>
2025-04-21 13:35:01 +0530

    cors wild card

--- cloudcontrol_widget_backend/settings.py
+++ cloudcontrol_widget_backend/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', 0))
 
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '127.0.0.1,localhost').split(",")
+# ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '127.0.0.1,localhost').split(",")
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '*').split(",")
 
 
 # Application definition
